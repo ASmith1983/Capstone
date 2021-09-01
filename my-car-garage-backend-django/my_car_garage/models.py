a750afb52ef0b3175bb9dabba355a72f57e649f6
Andoner <Andsmith316@gmail.com>
2021-09-01 15:14:41 -0700

    models.py 1st migrate

--- my-car-garage-backend-django/my_car_garage/models.py
+++ my-car-garage-backend-django/my_car_garage/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.name
+    
+class Vehicle(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.CharField(max_length=4, default='2021')
+    color = models.CharField(max_length=25)
+    odometer = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.make
