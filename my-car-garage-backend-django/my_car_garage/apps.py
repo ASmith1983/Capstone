c56a1be3e000b3bb30860da1816f46f077825765
Andoner <Andsmith316@gmail.com>
2021-08-31 23:24:07 -0700

    basic front/backend layout

---
+++ my-car-garage-backend-django/my_car_garage/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyCarGarageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_car_garage'
---
+++ my-car-garage-backend-django/settings.sql
@@ -0,0 +1,4 @@
+-- settings.sql
+CREATE DATABASE my_car_garage;
+CREATE USER a_user WITH PASSWORD 'password';
+GRANT ALL PRIVILEGES ON DATABASE my_car_garage TO a_user;
